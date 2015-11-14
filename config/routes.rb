10cc968e33f823885a18f0b74bf67c7ddd5f07fa
noboru-i <ishikura.noboru@gmail.com>
2015-11-14 21:23:39 +0900

    トップの作成・詳細の移動

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'detail#index'
+  root 'welcomes#index'
+
+  get 'details/:id' => 'details#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
