4b1bc1db6ee93c60711962faaa493815aeee92b1
noboru-i <ishikura.noboru@gmail.com>
2015-11-15 00:21:45 +0900

    詳細画面のデザイン仮当て

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,2 +1,10 @@
 class DetailsController < ApplicationController
+  def index
+    @hall = Hall.find_by_id(detail_params[:id])
+  end
+
+  private
+  def detail_params
+    params.permit(:id)
+  end
 end
